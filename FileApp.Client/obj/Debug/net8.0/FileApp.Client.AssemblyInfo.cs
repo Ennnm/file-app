7a08febacda699af35e6e60bbe9d14fa4edeb2ec
en <jiaen.1sc4@gmail.com>
2024-05-09 12:10:48 +0200

    build: build differences

--- FileApp.Client/obj/Debug/net8.0/FileApp.Client.AssemblyInfo.cs
+++ FileApp.Client/obj/Debug/net8.0/FileApp.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5cadbe8a32504877e274d6db5133531477fc037")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1751cd7f5f0382dd62361994e0f00e57edc1af2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
